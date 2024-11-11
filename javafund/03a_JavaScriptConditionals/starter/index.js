a1484be44a7b4ad276556c847d17023307558695
niamhpava <niamh.perry@avanade.com>
2024-11-11 15:55:56 +0000

    lab 3

--- javafund/03a_JavaScriptConditionals/starter/index.js
+++ javafund/03a_JavaScriptConditionals/starter/index.js
@@ -1,2 +1,13 @@
+let age = 25;
+
+if (age <= 17) {
+    console.log("underage");
+} else if (age <= 65) {
+    console.log("Insurable");
+} else {
+    console.log("out of range");
+}
+
+age <= 17 ? console.log("Underage") : console.log("18 or over");
 
 
