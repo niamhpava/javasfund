d3e147e85420870f7db507aae362d5a3c38ca127
niamhpava <niamh.perry@avanade.com>
2024-11-13 12:21:26 +0000

    chapter 10

--- javafund/10_JavaScriptStyles/starter/index.js
+++ javafund/10_JavaScriptStyles/starter/index.js
@@ -4,8 +4,38 @@ document.querySelector('button#bgColour').addEventListener('click', greenToPink)
 document.querySelector('button#fonts').addEventListener('click', tnrToArial);
 // Do not alter lines of code 2-4!
 
-function blueToRed() {}
+console.log(blueParagraph.style.color);
+console.dir(getComputedStyle(blueParagraph).color);
 
-function greenToPink() {}
+function blueToRed() {
+    blueParagraph.style.color = "red";
+    console.log(blueParagraph.style.color);
+    console.log(getComputedStyle(blueParagraph).color);
+    blueParagraph.textContent = blueParagraph.textContent.replace("blue", "red")
+}
 
-function tnrToArial() {}
+const greenParagraphs = document.querySelectorAll(".greenBg");
+
+console.log(greenParagraphs[0].style.backgroundColor);
+console.dir(getComputedStyle(greenParagraphs[0]).backgroundColor);
+
+function greenToPink() {
+    for(let greenParagraph of greenParagraphs) {
+        greenParagraph.className = 'hotpinkBg';
+        console.log(greenParagraph.style.backgroundColor);
+        console.log(getComputedStyle(greenParagraph).backgroundColor);
+        greenParagraph.textContent = greenParagraph.textContent.replace("green", "hotpink");
+    }
+}
+
+const tnrParagraph = document.querySelector("#tnrParagraph");
+
+console.log(tnrParagraph.style.fontFamily);
+console.dir(getComputedStyle(tnrParagraph).fontFamily);
+
+function tnrToArial() {
+    tnrParagraph.style.fontFamily = "arial";
+    console.log(tnrParagraph.style.fontFamily);
+    console.log(getComputedStyle(tnrParagraph).fontFamily);
+    tnrParagraph.textContent = tnrParagraph.textContent.replace("Times New Roman", "Arial");
+}
