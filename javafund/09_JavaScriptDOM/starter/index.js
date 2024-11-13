f172774d76478acffe0408e7c136e05df2ef319a
niamhpava <niamh.perry@avanade.com>
2024-11-13 10:51:25 +0000

    part 9

--- javafund/09_JavaScriptDOM/starter/index.js
+++ javafund/09_JavaScriptDOM/starter/index.js
@@ -1,1 +1,26 @@
 const colours = ['red', 'green', 'blue', 'orange', 'lime','olive', 'black', 'yellow'];
+
+function buildP(placeholder) {
+    let p = document.createElement('p');
+    let text = document.createTextNode('Have you tried turning it off and back on again?');
+    p.appendChild(text);
+    placeholder.appendChild(p);
+}
+
+buildP(document.querySelector('#placeholder'));
+
+function buildP2(placeholder, num) {
+    let i = 0;
+
+    do {
+        let p = document.createElement('p');
+        let text = document.createTextNode(`Have you tried turning it off and back on again?`);
+
+        p.appendChild(text);
+        p.style.color = colours[parseInt(Math.random() * colours.length)];
+        placeholder.appendChild(p);
+        i++;
+    } while (i < num);
+}
+
+buildP2(document.querySelector('#placeholder'), 6);
