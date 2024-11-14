df9bc92ab132d3e93086b4b682f1a0aa7c3c034b
niamhpava <niamh.perry@avanade.com>
2024-11-14 10:13:59 +0000

    task 13

--- javafund/13_JavaScriptModules/starter/index.js
+++ javafund/13_JavaScriptModules/starter/index.js
@@ -1,31 +1,7 @@
-const nameInput = document.querySelector('[name="name"]');
-const emailInput = document.querySelector('[name="email"]'); 
-
-const formSubmit = event => {
-    event.preventDefault();
-    alert(`The form was submitted.`);
-    for(let i = 0; i < 3;i++) {
-        console.log(event.target[i].value);
-    }
-    
-}
+import { formSubmit } from './formSubmit.js';
+import { nameInput, validateNameLength } from './validateName.js';
+import { emailInput, validateEmailFormat } from './validateEmail.js';
 
 document.querySelector('form').addEventListener('submit', formSubmit);
-
-const validateNameLength = event => {
-    if(event.target.value.length < 2) {
-        alert("Name not long enough");
-        nameInput.focus();
-    }
-}
-
 nameInput.addEventListener('change', validateNameLength);
-
-const validateEmailFormat = event => {
-    if(!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(event.target.value))) {
-        alert(`Email address not in a valid format`);
-        emailInput.focus();
-    }
-}
-
 emailInput.addEventListener('change', validateEmailFormat);
